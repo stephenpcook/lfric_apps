d0c0929918275f8132c5bd83340c1546b35bccc0
jamesbruten <enquiries@metoffice.gov.uk>
2024-05-15 14:23:34 +0000

    ticket:116 : matthewhambley : Galerkin projection depends on geometric constants
    git-svn-id: https://metomi/svn/lfric_apps.xm/main/trunk@1348 3896877f-8c05-4358-bc88-7947bb4308c7

--- dependencies.sh
+++ dependencies.sh
@@ -21,7 +21,7 @@
 # dependent changes should be updated to the revision at which those
 # changes were committed to the project's trunk
 
-export lfric_core_rev=50256
+export lfric_core_rev=50323
 export lfric_core_sources=
 export casim_rev=um13.5
 export casim_sources=
